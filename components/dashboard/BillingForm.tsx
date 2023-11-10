ca9285dd26894291d58480c5039f793bede30c0c
karimkhaled0 <kkmawe@gmail.com>
2023-11-10 07:13:11 +0200

    format billing form

--- components/dashboard/BillingForm.tsx
+++ components/dashboard/BillingForm.tsx
@@ -68,8 +68,11 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
               <p className="rounded-full text-xs font-medium">
                 {subscriptionPlan.isCanceled
                   ? "Your plan will be canceled on "
-                  : "Your plan renews on"}
-                {format(subscriptionPlan.stripeCurrentPeriodEnd!, "dd.MM.yyyy")}
+                  : "Your plan renews on "}
+                {format(
+                  subscriptionPlan.stripeCurrentPeriodEnd!,
+                  "dd.MMM.yyyy"
+                )}
                 .
               </p>
             ) : null}
