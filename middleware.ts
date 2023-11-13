bc6d4a29ac2a8b2b1097ee7dd075bf8727221b40
karimkhaled0 <kkmawe@gmail.com>
2023-11-13 03:50:32 +0200

    final

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default authMiddleware;
-
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard/:path*", "/auth-callback"],
 };
+
+export default authMiddleware;
